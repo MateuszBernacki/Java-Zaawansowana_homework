920d62627e1080b1d0ad0877d7f280e6f723539f
Mateusz <m.bernacki14@gmail.com>
2021-03-07 08:57:06 +0100

    TODO JAK DODAC DO METODY KOD ZLICZAJACY SŁIWA LICZY ILOŚĆ SŁÓW W PLIKU TASK14.TXT
    LICZY ILOŚĆ LINI W PLIKU TASK14.TXT
    TODO JAK PODAĆ ŚCIEŻKĘ DO PLIKU W IDE???

--- src/main/java/homework/task14/Task14A.java
+++ src/main/java/homework/task14/Task14A.java
@@ -1,6 +1,8 @@
 package homework.task14;
 
-import java.io.InputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * CZĘŚĆ 1
@@ -22,5 +24,35 @@ public class Task14A {
         InputStream input = Task14A.class.getResourceAsStream("../../task114.txt");
         //sekwencja ../ powoduje przejście o katalog wyżej
         //dwie sa konieczne ponieważ każdy pakiet to katalog, a mamy pakiet task14 w pakiecie homework
+
+        //TODO JAK DODAC DO METODY KOD ZLICZAJACY SŁIWA
+        //LICZY ILOŚĆ SŁÓW W PLIKU TASK14.TXT
+//            Scanner scanner = new Scanner(input);
+//            int countWords = 0;
+//            while (scanner.hasNext()) {
+//                scanner.next();
+//                countWords++;
+//            }
+//            System.out.println("Ilość słów to: " + countWords);
+//            scanner.close();
+//
+
+        //LICZY ILOŚĆ LINI W PLIKU TASK14.TXT
+        //TODO JAK PODAĆ ŚCIEŻKĘ DO PLIKU W IDE???
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader("c:\\data\\task14.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null){
+                lines.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie ma takiego pliku!");
+
+        } catch (IOException e) {
+            System.out.println("Bład odczytu, brak danych");
+        }
+        System.out.println("Ilosć lini: " +lines.size());
+        System.out.println("\b\b");
+
     }
 }
