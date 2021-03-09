6b65d8832d2ce6c5ed7efc8fff950b4ee7b8b42f
Mateusz <m.bernacki14@gmail.com>
2021-03-09 15:42:37 +0100

    Task14A zrobiony w 100%

--- src/main/java/homework/task14/Task14A.java
+++ src/main/java/homework/task14/Task14A.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * CZĘŚĆ 1
@@ -27,10 +29,13 @@ public class Task14A {
         //dwie sa konieczne ponieważ każdy pakiet to katalog, a mamy pakiet task14 w pakiecie homework
 
 
-        //DODANIE LINI ZA POMOCA BUFERA ORAZ SŁÓW ZA POMOCĄ BUFER SPLIT
+
+        //LICZY ILOŚĆ LINI W PLIKU TASK14.TXT
+        //TODO JAK PODAĆ ŚCIEŻKĘ DO PLIKU W IDE???
         List<String> lines = new ArrayList<>();
         List<String> numberOfWords = new ArrayList<>();
-        List<String> countWordsFourLeters =new ArrayList<>();
+        List<String> countWordsFourLeters = new ArrayList<>();
+        List<Integer> numbersList = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader("c:\\data\\task14.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -44,13 +49,44 @@ public class Task14A {
         } catch (IOException e) {
             System.out.println("Bład odczytu, brak danych");
         }
-        for (String numberOfWordsFourLeters : numberOfWords) {
-            if (numberOfWordsFourLeters.length() == 4){
-                countWordsFourLeters.add(numberOfWordsFourLeters);
+        for (String words : numberOfWords) {
+            if (words.length() == 4) {
+                countWordsFourLeters.add(words);
             }
         }
+        Pattern pattern = Pattern.compile("-?[0-9]+");
+        for (String numberOfWord : numberOfWords) {
+            Matcher m = pattern.matcher(numberOfWord);
+            while(m.find()){
+                int a = Integer.parseInt(m.group());
+                numbersList.add(a) ;
+            }
+        }
+
         System.out.println("Ilosć lini: " + lines.size());
         System.out.println("Ilość słów: " + numberOfWords.size());
         System.out.println("Ilość słów po 4 znaki to: " + countWordsFourLeters.size());
+        System.out.println("Ilość liczb: " +numbersList.size());
+
+        //CZĘŚĆ 2 ZADANIA WPROWADZENIE DANYCH DO PLIKU
+        try(PrintWriter print = new PrintWriter(new FileWriter("c:\\data\\out_task14.txt"))){
+            print.println("Ilosć lini: " + lines.size());
+            print.println("Ilość słów: " + numberOfWords.size());
+            print.println("Ilość słów po 4 znaki to: " + countWordsFourLeters.size());
+            print.println("Ilość liczb: " +numbersList.size());
+            print.println();
+            print.println("Lista wyrazów po 4 litery: ------------------------------");
+            for (String countWordsFourLeter : countWordsFourLeters) {
+                print.println(countWordsFourLeter);
+            }
+            print.println();
+            print.println("Lista wszystkich liczb: -----------------------------");
+            for (Integer integer : numbersList) {
+                print.println(integer);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
+
